3bdf61c3c0c5a2d76079e9109fc7dea78ce6182c
par5ul1 <elexir0@gmail.com>
2022-07-01 00:03:29 -0700

    Added comments to generator.

--- generator.js
+++ generator.js
@@ -3,21 +3,26 @@
  * @Date:   2022-06-29T23:40:40-07:00
  * @Filename: generator.js
  * @Last modified by:   par5ul1
- * @Last modified time: 2022-06-30T23:35:15-07:00
+ * @Last modified time: 2022-06-30T23:51:22-07:00
  */
 
 const pug = require('pug');
 const fs = require('fs');
 
+// Set up PugJS renderer
 const render = pug.compileFile('templates/parsuli/resume.pug');
 
+// Read the information and parse it as JSON
 const raw = fs.readFileSync("resume.json", (e) => console.log(e))
 const information = JSON.parse(raw);
 
+// Render the resume as HTML with Pug
 const resume = render(information);
 
-// fs.writeFile("public/index.html", index, (e) => console.log(e));
+// Create a 'public' directory if missing
 if (!fs.existsSync('public')){
     fs.mkdirSync('public');
 }
+
+// Write the resume to an HTML file, ready to be exported as a PDF
 fs.writeFile("public/resume.html", resume, (e) => console.log(e));
